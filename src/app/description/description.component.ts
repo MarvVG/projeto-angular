5d4404ff93a7a5cac734e2298fde57beaf0c28dd
Marcos Vinicius <vinicius.guimaraes@fiscal.io>
2025-04-27 14:14:51 -0300

    Melhoria das telas

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -3,6 +3,7 @@ import { ProductComponent } from '../product/product.component';
 import { ProductService } from '../product/product.service';
 import { Router } from '@angular/router';
 import { Product } from '../product/product.model';
+import { CartService } from '../cart/cart.service';
 @Component({
   selector: 'app-description',
   standalone: true,
@@ -10,9 +11,12 @@ import { Product } from '../product/product.model';
   styleUrl: './description.component.css'
 })
 export class DescriptionComponent {
-   constructor(private router: Router, private productService: ProductService) {}
+   constructor(private router: Router, private productService: ProductService, private cartService: CartService) {}
    product: Product = this.productService.getSelectedProduct()!;
    get imagePath() {
     return 'assets/products/' + this.product.avatar ;
+  }
+  addToCard(){
+    this.cartService.addProduct(this.product)
   }
 }
